97051c9a1ec3898a13493645604453acb5792fdc
Tao Xin <alexander.xin@gmail.com>
2024-05-08 15:52:20 -0700

    Initial commit

---
+++ schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS comment;
+CREATE TABLE IF NOT EXISTS comment (
+  uid TEXT NOT NULL,
+  nick TEXT NOT NULL,
+  mail TEXT NOT NULL,
+  mailMd5 TEXT NOT NULL,
+  link TEXT NOT NULL,
+  ua TEXT NOT NULL,
+  ip TEXT NOT NULL,
+  master INTEGER NOT NULL,
+  url TEXT NOT NULL,
+  href TEXT NOT NULL,
+  comment TEXT NOT NULL,
+  pid TEXT NOT NULL,
+  rid TEXT NOT NULL,
+  isSpam INTEGER NOT NULL,
+  created INTEGER NOT NULL,
+  updated INTEGER NOT NULL,
+  like TEXT NOT NULL,
+  PRIMARY KEY (url, created DESC)
+);
+
+DROP TABLE IF EXISTS config;
+CREATE TABLE IF NOT EXISTS config (
+  value TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS counter;
+CREATE TABLE IF NOT EXISTS counter (
+  url TEXT NOT NULL PRIMARY KEY,
+  title TEXT NOT NULL,
+  time INTEGER NOT NULL,
+  created INTEGER NOT NULL,
+  updated INTEGER NOT NULL
+);
